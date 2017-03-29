8e487e8a006fdd374f9d8fb91f8a995c7ca1bbcd
CooSamuel <cqs33@vip.qq.com>
2017-03-29 21:23:49 +0800

    Clion common

--- Assignment_3/Assignment_main.cpp
+++ Assignment_3/Assignment_main.cpp
@@ -1,4 +1,15 @@
 #include<iostream>
+
+class complexClass
+{
+    double x;
+    double y;
+public:
+    void operator = (complexClass a);
+    complexClass operator + (complexClass a);
+    complexClass operator - (complexClass a);
+
+};
 int main(int argc, char const *argv[]) {
     std::cout << "Hello world" << '\n';
     return 0;
